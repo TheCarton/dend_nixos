743deef9c0c998b4cfd53aedaab69a086407633f
TheCarton <theukearchy@gmail.com>
2026-05-06 16:05:26 -0400

    sddm a little better

--- modules/features/sddm.nix
+++ modules/features/sddm.nix
@@ -2,9 +2,25 @@
   flake.nixosModules.sddm =
     { pkgs, ... }:
     {
-      services.displayManager.sddm = {
-        enable = true;
-        wayland.enable = true;
+      qt.enable = true;
+      services.displayManager = {
+        # generic.preStart = "${pkgs.wlr-randr}/bin/wlr-randr --output HDMI-A-1 --off";
+        sddm = {
+          package = pkgs.kdePackages.sddm;
+          enable = true;
+          wayland.enable = true;
+          autoNumlock = true;
+          theme = "sddm-astronaut-theme";
+          extraPackages = [ pkgs.sddm-astronaut ];
+        };
+        defaultSession = "niri";
       };
+
+      environment.systemPackages = [
+        pkgs.kdePackages.qtmultimedia
+        pkgs.wlr-randr
+        pkgs.sddm-astronaut
+      ];
+
     };
 }
