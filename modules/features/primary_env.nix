7dd2f77db0d38dc973442a20a156d3ff13e80c74
TheCarton <theukearchy@gmail.com>
2026-05-28 20:06:17 -0400

    add anki

--- modules/features/primary_env.nix
+++ modules/features/primary_env.nix
@@ -25,6 +25,13 @@
         EDITOR = "hx";
       };
 
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      };
+
       services.syncthing = {
         enable = true;
         user = "luke";
@@ -47,6 +54,7 @@
 
       ## Add software below.
       environment.systemPackages = [
+        pkgs.anki
         pkgs.keepassxc
         pkgs.sshfs
         pkgs.chezmoi
